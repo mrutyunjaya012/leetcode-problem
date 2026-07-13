803c96c5a6c99100ddfa9bca8c4277c4d9305681
mrutyunjatya01 <mrutyunjaya407@gmail.com>
2026-07-13 16:19:26 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,19 @@
+// Last updated: 13/07/2026, 16:19:25
+class Solution {
+public:
+    // fibonacci series 
+    int climbStairs(int n) {
+        if (n<=2) return n;
+
+        int prev2=1;
+        int prev1=2;
+
+        for(int i=3; i<=n; i++){
+            int cur =prev1 + prev2;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return prev1;
+    }
+
+};
