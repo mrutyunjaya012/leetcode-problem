9568b548d34f5e0272b239a212a7a5184043419a
mrutyunjatya01 <mrutyunjaya407@gmail.com>
2026-07-13 16:18:30 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.cpp
@@ -0,0 +1,16 @@
+// Last updated: 13/07/2026, 16:18:30
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int>row;
+        long long ans=1;
+        row.push_back(1);
+
+        for(int i=1; i<=rowIndex; i++){
+            ans=ans*(rowIndex-i+1);
+            ans=ans/i;
+            row.push_back(ans);
+        }
+        return row;
+    }
+};
