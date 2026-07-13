a583f617430ff05f5850fad548411553c8f3c7cb
mrutyunjatya01 <mrutyunjaya407@gmail.com>
2026-07-13 16:12:49 +0530

    Create 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi

---
+++ 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi/2978-CheckIfStringsCanBeMadeEqualWithOperationsIi.cpp
@@ -0,0 +1,27 @@
+// Last updated: 13/07/2026, 16:12:48
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+    int even[26] = {0};
+    int odd[26] = {0};
+
+    for (int i = 0; i < s1.size(); i++) {
+        if (i % 2 == 0) {
+            even[s1[i] - 'a']++;
+            even[s2[i] - 'a']--;
+        } else {
+            odd[s1[i] - 'a']++;
+            odd[s2[i] - 'a']--;
+        }
+    }
+
+    // Check if all counts are zero
+    for (int i = 0; i < 26; i++) {
+        if (even[i] != 0 || odd[i] != 0)
+            return false;
+    }
+
+    return true;
+
+    }
+};
