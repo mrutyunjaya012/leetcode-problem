d0f3346c9a3009861757e5802e6d066b14320b4d
mrutyunjaya012 <mrutyunjaya407@gmail.com>
2026-07-15 16:47:43 +0530

    Create 456-132Pattern

---
+++ 456-132Pattern/456-132Pattern.cpp
@@ -0,0 +1,20 @@
+// Last updated: 15/07/2026, 16:47:42
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+
+        stack<int>st;
+        int third=INT_MIN;
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i]<third){
+                return true;
+            }
+            while(!st.empty() && nums[i]>st.top()){
+                third=st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
