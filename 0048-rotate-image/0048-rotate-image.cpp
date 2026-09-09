047cc5911d2b1c5f022fa6e14315a8cc5ee1bc59
mrutyunjaya012 <mrutyunjaya407@gmail.com>
2026-09-09 10:01:47 +0530

    Time: 0 ms (100%), Space: 10.2 MB (36.5%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,10 @@ public:
         int n=matrix.size();
         for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]); 
+                // don't need do transpose we just swap the elements (except main diagonal)
+                // first 2 - 4 , 3 - 7 , 6 - 8  then 
+                // reverse the row 
             }
         }
         for(int i=0; i<n;i++){
